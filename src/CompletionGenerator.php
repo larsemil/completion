6d77de9184552d030804a3520e4f6d6bfa062534
Emil Österlund <emil@dalnix.se>
2017-08-15 22:54:13 +0200

    Working prototype

--- src/CompletionGenerator.php
+++ src/CompletionGenerator.php
@@ -6,7 +6,7 @@ namespace larsemil\completion;
 // credit goes to CLIFramework
 // https://github.com/c9s/CLIFramework/blob/master/src/Completion/BashGenerator.php
 
-class generator
+class CompletionGenerator
 {
 
     protected $autocompletes;
@@ -21,7 +21,7 @@ class generator
             $this->cmdname = $_cmdname;
             $this->autocompletes = $_autocompletes;
 
-            $this->createBashCompletionScript()
+            $this->createBashCompletionsScript()
                 ->runInBash();
         }
 
@@ -29,10 +29,11 @@ class generator
 
     public function createBashCompletionsScript()
     {
-        $this->bashfile = file_get_contents('bash.template');
-        $this->bashfile = str_replace('#CMDNAME#',$this->cmdname);
 
-        $this->bashfile = str_replace('#AVAILABLEARGUMENTS#', $this->generateArgumentsString());
+        $this->bashfile = file_get_contents(__DIR__.'/bash.template');
+        $this->bashfile = str_replace('#CMDNAME#',$this->cmdname, $this->bashfile);
+
+        $this->bashfile = str_replace('#AVAILABLEARGUMENTS#', $this->generateArgumentsString(), $this->bashfile);
 
         $this->bashfile = '<< EOF'.PHP_EOL.$this->bashfile.PHP_EOL.'EOF';
         return $this;
@@ -40,7 +41,16 @@ class generator
     }
 
     public function runInBash(){
-        shell_exec($this->bashfile);
+
+
+        //echo shell_exec('bash --noprofile --norc');
+        //echo shell_exec('   echo $0');
+        //echo shell_exec('whoami');
+        //echo $this->bashfile;
+        $f = fopen('dump.bash','w');
+        fwrite($f, $this->bashfile);
+        fclose($f);
+        shell_exec('bash '.$this->bashfile);
     }
 
 
